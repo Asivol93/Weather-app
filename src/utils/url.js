d1de3e93934edf9103881771b3062ba53fc1967a
Lovisa Carling <lovisa.carling@gmail.com>
2022-02-23 22:10:35 +0100

    changed location of api key

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,3 +1,3 @@
-const API_KEY = 'da4ce88f43bcb670fa90bad02541759e'
+const API_KEY = process.env.REACT_APP_API_KEY
 export const API_URL = (slug) =>
   `api.openweathermap.org/data/2.5/weather?q=${slug}&appid=${API_KEY}`
