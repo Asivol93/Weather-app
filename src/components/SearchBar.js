fad31822b330af8ca96c5b7c96df4c06dba593f3
Lovisa Carling <lovisa.carling@gmail.com>
2022-02-24 18:48:09 +0100

    fetch works

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
-export const SearchBar = () => {
+export const SearchBar = ({
+  onLocationSubmit,
+  searchValue,
+  setSearchValue,
+}) => {
   return (
-    <form>
-      <input type='text' />
+    <form onSubmit={onLocationSubmit}>
+      <label htmlFor='searchbar'>Location:</label>
+      <input
+        id='searchbar'
+        type='text'
+        placeholder='City'
+        required
+        value={searchValue}
+        onChange={(event) => setSearchValue(event.target.value)}
+      />
       <button type='submit'>Search</button>
     </form>
   )
--- src/components/WeatherList.js
+++ src/components/WeatherList.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { SearchBar } from './SearchBar'
+import { API_URL } from '../utils/url'
 
 const WeatherSection = styled.section`
   width: 90%;
@@ -9,10 +10,56 @@ const WeatherSection = styled.section`
 `
 
 export const WeatherList = () => {
+  const [location, setLocation] = useState([])
+  const [searchValue, setSearchValue] = useState('')
+  const [error, setError] = useState('')
+
+  const isNewLocation = (locationId) => {
+    const duplicate = location.filter((item) => item.id === locationId)
+    return duplicate.length === 0
+  }
+
+  const onLocationSubmit = (event) => {
+    event.preventDefault()
+    setError('')
+
+    const options = {
+      method: 'GET',
+    }
+
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=da4ce88f43bcb670fa90bad02541759e`,
+      options
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.cod === 200 && isNewLocation(data.id)) {
+          setLocation([data, ...location])
+        } else if (data.cod === '404') {
+          setError(data.message)
+        } else {
+          console.log(data)
+        }
+      })
+    // searchValue('')
+  }
+
   return (
     <WeatherSection>
       <h1>Weather app</h1>
-      <SearchBar />
+      <SearchBar
+        onLocationSubmit={onLocationSubmit}
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
+      {error && <p>No location found...</p>}
+      {location.map((location) => (
+        <div key={location.id}>
+          <p>{location.name}</p>
+          <p>{location.weather[0].main}</p>
+          <p>{location.main.temp}</p>
+        </div>
+      ))}
     </WeatherSection>
   )
 }
